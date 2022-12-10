637add3fcde0e1863bdb769d8c9d0d8f4f02ad99
kimwanyoung <dhks2869@gmail.com>
2022-12-10 17:32:42 +0900

    ADD: 영화디테일 레이아웃 좋아요 구현 완료"

--- front/src/pages/Details/Detail.js
+++ front/src/pages/Details/Detail.js
@@ -2,11 +2,10 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 import styled from "styled-components";
 import { useState, useEffect } from "react";
-import {
-    getAccessToken
-} from "../../storage/Cookie";
+import { getAccessToken } from "../../storage/Cookie";
 import axios from "axios";
 import { newAccessToken } from "../../module/refreshToken";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 axios.defaults.headers.common["Authorization"] = getAccessToken();
 
 const Detail = () => {
@@ -144,17 +143,68 @@ const Detail = () => {
         <div>
             {movieData && (
                 <>
-                    <h1>{movieTitle}</h1>
                     <DetailWrapper>
                         <ImageWrapper src={movieData.postLink} />
                         <MovieInfo>
                             <TitleLike>
-                                <Title>{movieTitle}</Title>
-                                <Line/>
-                                <LikeBtn />
+                                <Title>
+                                    <h2>{movieTitle} | </h2>
+                                    <TitleSmall>
+                                        <div />
+                                        <p>{movieTitle}</p>
+                                    </TitleSmall>
+                                </Title>
+                                <LikeBtn onClick={handleLikeButton}>
+                                    {isLike ? <FullHeart /> : <Heart />}
+                                    <p>좋아요</p>
+                                </LikeBtn>
                             </TitleLike>
+                            <MovieDetailInfo>
+                                <p>12세 이상 관람가 . 10월 12일 . 2시간20분</p>
+                                <p>#모헝 #스릴러 #판타지</p>
+                            </MovieDetailInfo>
+                            <Synopsis>
+                                <p>{movieData.synopsis}</p>
+                            </Synopsis>
+                            <ActorAndReview>
+                                <p>
+                                    출연 |{" "}
+                                    {movieData?.actorList
+                                        ?.slice(0, 4)
+                                        .map((props) => props.actorName + " ")}
+                                </p>
+                                <ReviewBtnWrapper>
+                                    <ReviewBtn>리뷰 쓰기 &#62;</ReviewBtn>
+                                    <ReviewBtn>퀴즈 풀기 &#62;</ReviewBtn>
+                                </ReviewBtnWrapper>
+                            </ActorAndReview>
                         </MovieInfo>
                     </DetailWrapper>
+                    <UnderContentWrapper>
+                        <LikeGraph>
+                            <TestGraph/>
+                        </LikeGraph>
+                        <VideoWrapper>
+                            <VideoTitle>예고편</VideoTitle>
+                            <VideoOutLine>
+                                <Video/>
+                                <Video/>
+                                <Video/>
+                            </VideoOutLine>
+                        </VideoWrapper> 
+                        <GreenLine/>
+                        <VideoWrapper>
+                            <StillImageTitle>
+                                <p>Photo</p>
+                                <h3>더보기 &#62;</h3>
+                            </StillImageTitle>
+                            <StillImageWrapper>
+                                {movieData.stillImage?.slice(0,6).map((props, idx) => (
+                                    <img src={props} key={idx} alt="still"/>
+                                ))}
+                            </StillImageWrapper>
+                        </VideoWrapper>
+                    </UnderContentWrapper>                  
                 </>
             )}
         </div>
@@ -169,7 +219,6 @@ const DetailWrapper = styled.div`
     align-items: center;
     width: 100vw;
     height: 80vh;
-    border: 1px solid white;
 `;
 
 const ImageWrapper = styled.img`
@@ -180,28 +229,212 @@ const ImageWrapper = styled.img`
 const MovieInfo = styled.div`
     width: 25rem;
     height: 33rem;
-    border: 1px solid white;
-`
+    margin-left: 2rem;
+`;
 const TitleLike = styled.div`
     display: flex;
     justify-content: space-between;
     align-items: center;
     width: 100%;
-`
+`;
 
 const LikeBtn = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
     width: 5rem;
     height: 2.5rem;
-    border: 1px solid white;
+    border-radius: 0.3rem;
+    background-color: #03af59;
+    p {
+        color: ${(props) => (props.isLike ? "red" : "white")};
+        font-size: 0.8rem;
+    }
+`;
+
+const Title = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 1.3rem;
+    h2 {
+        font-size: 1.7rem;
+        font-weight: 700;
+        color: white;
+    }
+`;
+
+const TitleSmall = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    flex-direction: column;
+    width: 3rem;
+    height: 100%;
+    p {
+        font-size: 0.5rem;
+        color: white;
+    }
+`;
+
+const Heart = styled(AiOutlineHeart)`
+    width: 1.2rem;
+    height: 1.2rem;
+    color: white;
+    margin-right: 0.3rem;
+`;
+
+const FullHeart = styled(AiFillHeart)`
+    width: 1.2rem;
+    height: 1.2rem;
+    color: red;
+    margin-right: 0.3rem;
+`;
+
+const MovieDetailInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 5rem;
+    padding-top: 0.8rem;
+    border-bottom: 1px solid #03af59;
+    p {
+        margin-bottom: 0.3rem;
+        font-size: 0.9rem;
+        color: white;
+    }
+`;
+
+const Synopsis = styled.div`
+    width: 100%;
+    height: 15rem;
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    border-bottom: 1px solid #03af59;
+
+    p {
+        font-size: 0.9rem;
+        color: white;
+    }
+`;
+
+const ActorAndReview = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 100%;
+    height: 10.5rem;
+    padding-top: 1rem;
+    p {
+        color: white;
+    }
+`;
+
+const ReviewBtn = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 25%;
+    height: 2.5rem;
+    border-radius: 1rem;
+    margin: 0 1rem 0 1rem;
+    background-color: #e8ac4b;
+    color: white;
+`;
+
+const ReviewBtnWrapper = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    width: 100%;
+`;
+
+const UnderContentWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content:center;
+    align-items: center;
+    width: 100%;
+`
+const TestGraph = styled.div`
+    width: 90%;
+    height: 90%;
+    background-color:#535353;
+    margin: 0 auto;
 `
 
-const Title = styled.h2`
-    font-size: 1.7rem;
-    font-weight: 700;
+const LikeGraph = styled.div`
+    width: 50%;
+    height: 20rem;
+    border-bottom: 1px solid #03af59;
+`
+const VideoWrapper =styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    width: 60%;
+    height: 20rem;
+    margin-top: 2rem;
+`
+
+const GreenLine = styled.div`
+    width: 30rem;
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+    border:0.5px solid #03af03;
+`
+
+const VideoTitle = styled.h2`
+    width: 100%;
+    font-size: 1.5rem;
+    font-weight: 600;
     color: white;
 `
-const Line = styled.div`
-    width:1px;
-    height: 2.5rem;
+const Video = styled.div`
+    width: 30%;
+    height: 10rem;
     border: 1px solid white;
+    margin-left:1rem;
+`
+
+const VideoOutLine = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+`
+
+const StillImageWrapper = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+    margin-left: 7rem;
+    img {
+        width: 15rem;
+        height: 13rem;
+        margin-right:1rem;
+        margin-top: 1rem;
+    }
+`
+
+const StillImageTitle = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items:center;
+    width: 100%;
+    height: 3rem;
+    padding-left: 1rem;
+    padding-right: 1rem;
+
+    p {
+        font-size: 1.5rem;
+        font-weight: 600;
+        color: white;
+    }
+
+    h3{
+        font-size: 1.3rem;
+        font-weight: 600;
+        color: #03af03;
+    }
 `
