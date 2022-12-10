80b61c67b3bb1624422095c96fdffb2cd0e60f70
kimwanyoung <dhks2869@gmail.com>
2022-12-10 16:12:30 +0900

    pull r

--- front/src/pages/Details/Detail.js
+++ front/src/pages/Details/Detail.js
@@ -147,32 +147,13 @@ const Detail = () => {
                     <h1>{movieTitle}</h1>
                     <DetailWrapper>
                         <ImageWrapper src={movieData.postLink} />
-                        <DetailInfoWrapper>
-                            <DetailTitle>
-                                {movieData.title}
-                                <LikeBox
-                                    isLike={isLike}
-                                    onClick={handleLikeButton}
-                                >
-                                    좋아요
-                                </LikeBox>
-                                <LikeBox
-                                    isLike={myMovie}
-                                    onClick={handleMyMovieButton}
-                                >
-                                    찜 하기
-                                </LikeBox>
-                            </DetailTitle>
-                            <DetailDesc>
-                                <p>{movieData.synopsis}</p>
-                            </DetailDesc>
-                        </DetailInfoWrapper>
-                        {/* {movieData.stillImage &&
-                        movieData.stillImage
-                            .slice(0, 9)
-                            .map((props) => (
-                                <ImageWrapper src={props} alt="still image" />
-                            ))} */}
+                        <MovieInfo>
+                            <TitleLike>
+                                <Title>{movieTitle}</Title>
+                                <Line/>
+                                <LikeBtn />
+                            </TitleLike>
+                        </MovieInfo>
                     </DetailWrapper>
                 </>
             )}
@@ -184,19 +165,11 @@ export default Detail;
 
 const DetailWrapper = styled.div`
     display: flex;
-    justify-content: space-around;
+    justify-content: center;
     align-items: center;
     width: 100vw;
     height: 80vh;
     border: 1px solid white;
-`;
-
-const DetailInfoWrapper = styled.div`
-    display: flex;
-    justify-content: space-around;
-    flex-direction: column;
-    width: 40%;
-    height: 33rem;
 `;
 
 const ImageWrapper = styled.img`
@@ -204,28 +177,31 @@ const ImageWrapper = styled.img`
     height: 33rem;
 `;
 
-const DetailTitle = styled.h2`
+const MovieInfo = styled.div`
+    width: 25rem;
+    height: 33rem;
+    border: 1px solid white;
+`
+const TitleLike = styled.div`
     display: flex;
     justify-content: space-between;
-    font-size: 1.5rem;
-    color: white;
-`;
-
-const DetailDesc = styled.div`
-    display: flex;
     align-items: center;
-    justify-content: center;
     width: 100%;
-    height: 12rem;
-    padding: 1rem;
-    background-color: rgba(205, 207, 204, 0.7);
-`;
+`
 
-const LikeBox = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
+const LikeBtn = styled.div`
     width: 5rem;
-    height: 3rem;
-    background-color: ${(props) => (props.isLike ? "red" : "rgba(0,0,0,0.3)")};
-`;
+    height: 2.5rem;
+    border: 1px solid white;
+`
+
+const Title = styled.h2`
+    font-size: 1.7rem;
+    font-weight: 700;
+    color: white;
+`
+const Line = styled.div`
+    width:1px;
+    height: 2.5rem;
+    border: 1px solid white;
+`
