e68d78b8dfbd1d5b4c10963830f9db7c3d8a3a5b
Jae woo kim <ta3844@naver.com>
2022-12-14 13:35:05 +0900

    ADD:Feat:404 Page incomplete

--- back/src/main/java/com/movie/back/BackApplication.java
+++ back/src/main/java/com/movie/back/BackApplication.java
@@ -3,14 +3,19 @@ package com.movie.back;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class BackApplication {
+public class BackApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackApplication.class, args);
+           ApplicationContext ctx =  SpringApplication.run(BackApplication.class, args);
+           DispatcherServlet dispatcherServlet = (DispatcherServlet)ctx.getBean("dispatcherServlet");
+           dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
     }
 
 }
